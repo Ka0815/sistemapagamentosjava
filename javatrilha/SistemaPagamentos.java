0a22c9b68bbeecf0642c55c4ce9ac78d7dd59d79
Karoline Rayssa <karol.rayssa0102@gmail.com>
2026-04-14 14:41:52 -0300

    Update SistemaPagamentos.java

--- javatrilha/SistemaPagamentos.java
+++ javatrilha/SistemaPagamentos.java
@@ -21,7 +21,7 @@ public class SistemaPagamentos {
                     break;
 
                 case "2":
-                    Pagamentos(entrada);
+                    Pagamentos(colaboradores);                    
                     break;
 
                 case "3":
@@ -54,53 +54,65 @@ public class SistemaPagamentos {
         int numeroregistro = entrada.nextInt();
         entrada.nextLine();
 
-        System.out.println("\nCadastrado com sucesso!");
-        Colaborador c = new Colaborador(nomecplt, numeroregistro );
-lista.add(c);
-    }
+        System.out.print("Digite o seu tipo de colaborador: ");
+        String tipo = entrada.nextLine();
+        entrada.nextLine();
 
-    public static void Pagamentos(Scanner entrada) {
-        System.out.println("1 - Padrão");
-        System.out.println("2 - Comissionado");
-        System.out.println("3 - Produção");
+        Colaborador c = new Colaborador(nomecplt, numeroregistro, tipo);
 
-        int salariobase = 2000;
-
-        System.out.print("Digite o tipo de funcionário: ");
-        String funcio = entrada.nextLine();
-
-        if (funcio.equals("1")) {
-            System.out.println("Seu salário é: " + salariobase);
-
-        } else if (funcio.equals("2")) {
-            System.out.print("Digite quanto você vendeu: ");
-            int vendas = entrada.nextInt();
+        if(tipo.equals("2")){
+            System.out.print("Digite o valor das vendas: ");
+            c.vendas = entrada.nextDouble();
 
             System.out.print("Digite o percentual: ");
-            double percentual = entrada.nextDouble();
+            c.percentual = entrada.nextDouble();
             entrada.nextLine();
-
-            double comissao = (vendas * percentual) / 100;
-            double salariototal = salariobase + comissao;
-
-            System.out.println("Seu salário total é: " + salariototal);
-
-        } else if (funcio.equals("3")) {
-            System.out.print("Digite o valor por peça: ");
-            double valorpeca = entrada.nextDouble();
-
+        } else if(tipo.equals("3")){
             System.out.print("Digite a quantidade produzida: ");
-            int qtdprod = entrada.nextInt();
-            entrada.nextLine(); 
+            c.quantidade = entrada.nextInt();
 
-            double bonus = valorpeca * qtdprod;
-            double salariotot = salariobase + bonus;
-
-            System.out.println("Seu salário total é: " + salariotot);
-
-        } else {
-            System.out.println("Opção inválida, tente novamente!");
+            System.out.print("Digite o valor por peça: ");
+            c.valorPeca = entrada.nextDouble();
+            entrada.nextLine();
         }
+lista.add(c);
+   System.out.println("\nCadastrado com sucesso!"); 
+   
+}
+
+    public static void Pagamentos(ArrayList<Colaborador> lista) {
+        
+        int salariobase = 2000;
+
+        if(lista.isEmpty()){
+            System.out.println("Nenhum colaborador cadastrado.");
+            return;
+        }
+        System.out.println("\nPagamentos dos colaboradores:");
+        for(Colaborador c : lista){
+            double pagamento = 0;
+
+            switch(c.tipo){
+                case "1":
+                    pagamento = salariobase;
+                    break;
+
+                case "2":
+                    pagamento = salariobase + (c.vendas * (c.percentual / 100));
+                    break;
+
+                case "3":
+                    pagamento = salariobase + (c.quantidade * c.valorPeca);
+                    break;
+            default:
+                System.out.println("Colaborador não encontrado");
+                continue;
+            }
+           
+            System.out.println("Nome: " + c.nomecplt + " - Pagamento: R$ " + String.format("%.2f", pagamento));
+        }
+
+        
     }
 
     public static void Listacolaboradores(ArrayList<Colaborador> lista) {
@@ -110,7 +122,7 @@ lista.add(c);
             System.out.println("Nenhum colaborador cadastrado.");
         } else {
             for (Colaborador c : lista) {
-               System.out.println("Nome: " + c.nomecplt+ "Registro:"+ c.numeroregistro );
+               System.out.println("Nome: " + c.nomecplt+ "\nRegistro: " + c.numeroregistro+ "\nTipo: " + c.tipo);
             }
             }
         }
